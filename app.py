b2c849ae75557497d103c622c7cf627bf6f8676b
li jieeeee <153187094+LEE-CHIEN-AN@users.noreply.github.com>
2025-07-25 04:09:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from supabase import create_client
 from datetime import datetime, timedelta, timezone
 # 字型設定（針對 Windows 中文支援）
 import matplotlib
-matplotlib.rc('font', family='Noto Sans CJK TC')
 
 # ---------- Supabase 設定 ----------
 # Supabase 設定
