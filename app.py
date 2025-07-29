1b9b066f15d849f2b716651eaba948f9d2255ae8
li jieeeee <153187094+LEE-CHIEN-AN@users.noreply.github.com>
2025-07-30 05:07:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from supabase import create_client
 from datetime import datetime, timedelta, timezone
 # 字型設定（針對 Windows 中文支援）
 import matplotlib
-
+import plotly.express as px
 # ---------- Supabase 設定 ----------
 # Supabase 設定
 # === Supabase 設定（從 GitHub Secrets 環境變數取得） ===
