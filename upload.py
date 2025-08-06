da589b7a028862457910c4a3eb260047c27b53a0
li jieeeee <153187094+LEE-CHIEN-AN@users.noreply.github.com>
2025-08-06 17:05:55 +0800

    Update upload.py

--- upload.py
+++ upload.py
@@ -115,6 +115,35 @@ def get_thingspeak_604center_data():
     except Exception as e:
         print("❌ 無法取得 ThingSpeak 資料：", e)
         return None
+
+def get_thingspeak_604window_data():
+    CHANNEL_ID = "3027253"
+    READ_API_KEY = "G9XE5ZK8PCDHADKC"
+    FIELD_URL = f"https://api.thingspeak.com/channels/{CHANNEL_ID}/feeds.json"
+    params = {"api_key": READ_API_KEY, "results": 1}
+
+    try:
+        r = requests.get(FIELD_URL, params=params, timeout=5)
+        r.raise_for_status()
+        feed = r.json()["feeds"][0]
+
+        return {
+            "name": "wiolink window",
+            "humidity": int(feed["field2"]),
+            "light_intensity":  None,
+            "motion_detected": None,
+            "celsius_degree": float(feed["field1"]),
+            "mag_approach":None,
+            "dust": None,
+            "touch": None,
+            "pm1_0_atm": int(feed["field4"]),
+            "pm2_5_atm": int(feed["field5"]),
+            "pm10_atm": int(feed["field6"])
+        }
+
+    except Exception as e:
+        print("❌ 無法取得 ThingSpeak 資料：", e)
+        return None
         
 # === 將單筆感測資料上傳至 Supabase ===
 def upload_to_supabase(data):
@@ -145,3 +174,9 @@ if __name__ == "__main__":
     vlabcenter_data = get_thingspeak_604center_data()
     if vlabcenter_data:
         upload_to_supabase(vlabcenter_data)
+
+    # 上傳 ThingSpeak 604 window資料
+    vlabwindow_data = get_thingspeak_604window_data()
+    if vlabcenter_data:
+        upload_to_supabase(vlabwindow_data)
+
